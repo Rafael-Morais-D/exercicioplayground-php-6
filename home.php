6059b967c504c72e6e0896d913dad52a35b59565
RafaelMorais <rafaelmorais.design@gmail.com>
2020-05-05 11:49:51 -0300

    Estudando sobre a variavel superglobal GET

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump($_GET);
+
+?>
+
+<form class="" action="home.php" method="GET">
+    <input type="text" name="nome" value="">
+    <input type="text" name="sobrenome" value="">
+    <input type="number" name="idade" value="">
+
+    <button type="submit" name="button"> Enviar </button>
+</form>
+
+<?php
+
+echo "<h1>" . $_GET['nome'] . " " . $_GET['sobrenome'] . " tem " . $_GET['idade'] . " anos.";
+
+?>
